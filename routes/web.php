aa41b8d84fca2fcc9314aafb4ea4dad55e989065
cris30000 <crisamboni0724@gmail.com>
2025-11-04 17:30:06 -0500

    Mi Primer commit de  prueba en rama Cris

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+//este es una prueba de conexion entre ramas
+route::get('/inicio', function () {
+    return view('inicio');
+});
